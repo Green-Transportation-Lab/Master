693663eca5829d0bd83bcce443c33d8fd8548a3b
Alex-GTL <33165639+Alex-GTL@users.noreply.github.com>
2017-12-04 14:27:29 -0800

    Update Gen_Class.py

--- Gen_Class.py
+++ Gen_Class.py
@@ -1,9 +1,9 @@
 class Generator:
 
-name = "" 
-cur_P_out = 0 #kW
+  name = "" 
+  cur_P_out = 0 #kW
 
-#Generation Forecast
-Peak_time = 0
-hour_power = 0
-day_power = 0
+   #Generation Forecast
+  Peak_time = 0
+  hour_power = 0
+  day_power = 0
